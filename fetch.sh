0828dce8f3f62b75a6cf9e830cb2de9e60cc2a8a
Yuan <yw.johnny@gmail.com>
2017-04-03 18:16:04 +0100

    Add shell script for fetching segments

---
+++ fetch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  count=1
+else count=$1
+fi
+
+for i in $(seq 1 $count); do
+  echo $i
+  bin/nutch generate crawl/crawldb crawl/segments
+  s1=`ls -d crawl/segments/2* | tail -1`
+  echo $s1
+  bin/nutch fetch $s1
+  bin/nutch parse $s1
+  bin/nutch updatedb crawl/crawldb $s1
+done
