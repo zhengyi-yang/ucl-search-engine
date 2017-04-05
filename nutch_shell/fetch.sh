5659758408e21a43101856e16e0f61e51f89b46b
Yuan <yw.johnny@gmail.com>
2017-04-05 17:41:03 +0100

    Add shell script for nutch

--- nutch_shell/fetch.sh
+++ nutch_shell/fetch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash         
 
 if [ -z "$1" ]; then
   count=1
@@ -6,10 +6,9 @@ else count=$1
 fi
 
 for i in $(seq 1 $count); do
-  echo $i
+  echo --- Iteration $i ---
   bin/nutch generate crawl/crawldb crawl/segments
   s1=`ls -d crawl/segments/2* | tail -1`
-  echo $s1
   bin/nutch fetch $s1
   bin/nutch parse $s1
   bin/nutch updatedb crawl/crawldb $s1
