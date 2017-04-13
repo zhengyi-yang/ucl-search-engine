d4572525673f636c0d59b93763e61ab9181f56df
CEM ÖZGÜR <cemozgur@CEMs-MacBook-Pro.local>
2017-04-13 10:58:30 +0100

    Added getNumberOfNodes and general structure of methods.

--- apache-nutch-1.12/src/java/org/apache/nutch/scoring/webgraph/PageRank.java
+++ apache-nutch-1.12/src/java/org/apache/nutch/scoring/webgraph/PageRank.java
@@ -16,7 +16,9 @@ import org.apache.hadoop.util.StringUtils;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.nutch.util.NutchConfiguration;
-
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 public class PageRank extends Configured implements Tool {
   private static final Logger LOG = LoggerFactory
@@ -36,12 +38,43 @@ public class PageRank extends Configured implements Tool {
   public PageRank(Configuration conf) {
     super(conf);
   }
+	
+
+  
+    
+  private int getNumberOfNodes(FileSystem fs, Path webGraphDb) throws IOException {
+
+    String numberOfNodes = "";
+      
+    Path tempDir = new Path(webGraphDb, "tempCounterDir");
+    FSDataInputStream readNumberOfNodes = fs.open(new Path(tempDir, "part-00000"));
+    BufferedReader buffer = new BufferedReader(new InputStreamReader(readNumberOfNodes));
+    String numberOfLines = buffer.readLine();
+    buffer.close();
+    readNumberOfNodes.close();
+
+    if (numberOfLines == null || numberOfLines.length() == 0) {
+      fs.delete(tempDir, true);
+      throw new IOException("WebGraph is empty!");
+    }  
+      
+    fs.delete(tempDir, true);
+    numberOfNodes = numberOfLines.split("\\s+")[1];
+    
+    return Integer.parseInt(numberOfNodes);  
+
+  }
+	
+	
+  private boolean isConverged(){
+      
+      return true;
+  }
+      
+  
     
   /**
-   * Runs the complete link analysis job. The complete job determins rank one
-   * score. Then runs through a given number of invert and analyze iterations,
-   * by default 10. And finally replaces the NodeDb in the WebGraph with the
-   * link rank output.
+   * This is the PageRank link analsis method.
    * 
    * @param webGraphDb
    *          The WebGraph to run link analysis on.
@@ -50,7 +83,31 @@ public class PageRank extends Configured implements Tool {
    *           If an error occurs during link analysis.
    */
   public void analyze(Path webGraphDb) throws IOException {
-    
+
+    Configuration conf = getConf();
+    FileSystem fs = FileSystem.get(conf);
+
+    if (!fs.exists(linkRank)) {
+      fs.mkdirs(linkRank);
+    } 
+	  
+    //Initialise the PageRank scores of the webgraph
+    int numberOfNodes = getNumberOfNodes(fs, webGraphDb);
+    iniitialiseScores(wgNodeDb, nodeDb);
+    float firstRankScore = (1f / (float) numberOfNodes);
+
+    while(isConverged==false){
+        //Apply the algorithm here
+        
+    }
+	  
+	  
+	  
+
+	  
+    // replace the NodeDb in the WebGraph with the final output of analysis
+    FSUtils.replace(fs, wgNodeDb, nodeDb, true);
+
   }
 
   public static void main(String[] args) throws Exception {
